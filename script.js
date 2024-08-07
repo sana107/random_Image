cf1415cbbacdadd9f33b3127ce8ce62b99db39b3
sana107 <mahaksanole13@gmail.com>
2024-08-07 22:06:35 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const btn = document.getElementById("btn");
+// btn.addEventListener("click", refresh);
+
+function refresh(){
+
+    window.location.reload();
+}
+
+
+btn.addEventListener("click", refresh);
